3bf94afc1b5b941c2aa3390fbb355a26e14f2400
DarwinMo <mohamed.abdessalame@outlook.fr>
2025-11-20 19:32:20 +0100

    pagerank algo

--- pagerank.py
+++ pagerank.py
@@ -1,58 +1,55 @@
 import numpy as np
-import matplotlib
-A1 = np.matrix([0,0,1,1/2],[1/3,0,0,0],[1/3,1/2,0,1/2],[1/3,1/2,0,0])
-A2 = np.matrix([0,1,0,0,0],[1,0,0,0,0],[0,0,0,1,0],[0,0,1,0,0],[0,0,1,1,0])
 
-#cette fonction est quasiment impossible à se reproduire
-def vecteur_de_score(A,a) :
-    S = np.ones(len(A))/(len(A))
-    return (1-a)*A + a*S
-#meilleur solution
-def score_dynamique(A,a,iter_max) :
+def google_matrix(A, a):
+    n = A.shape[0]
+    S = np.ones((n, n)) / n
+    return (1 - a) * A + a * S
+
+
+
+def vecteur_de_score(A, a):
+    G = google_matrix(A, a)
+    vp, vecp = np.linalg.eig(G)
+    indice_rho = np.argmax(vp)
+    x = vecp[:, indice_rho]
+    x = x / np.sum(x)
+    return np.real(x)
+
+
+
+def score_dynamique(A, a, iter_max=300, eps=1e-3):
     n = len(A)
-    z = np.ones(1,n)/n
-    for i in range(0,iter_max + 1) :
-        z1 = np.dot(A,z)
-        if np.all(np.fabs(z-z1)<1e-3) :
+    G = google_matrix(A, a)
+    z = np.ones((n, 1)) / n
+    for i in range(iter_max + 1):
+        z1 = np.dot(G,z)
+        if np.all(np.abs(z1 - z) < eps):
             break
         z = z1
+    z = z / np.sum(z)
     return z
 
-#voir l'efficacité des convergences des suites utilisées precedement
-
-def erreur_de_score(A,a) :
-    n = len(A)
-    z = np.ones(1,n)/n
-    for i in range(301) :
-        z1 = np.dot(A,z)
-        if np.all(np.fabs(z-z1)<1e-3) :
-                break
-        z = z1
-
-
-    x = np.random.ranf((n,1)) / np.sum(n)
-    erreur = np.zeros(300)
-    for k in range(301) :
-        erreur[k] = np.sum(np.abs(z-x))
-        z = np.dot(A,z)
-
-    return erreur
 
 
 
-#erreur1 = erreur_de_score(A1,0.15)
-erreur2 = erreur_de_score(A2,0.15)
-plt.figure(figsize=(10,6))
-#plt.semilogy(erreur1,label="miniwebA")
-plt.semilogy(erreur,label="miiniwebB")
-plt.xlabel("Itérations")
-plt.ylabel("Erreur (échelle logarithmique)")
-plt.show
+if __name__ == "__main__":
+    A1 = np.array([
+        [0, 0, 1, 1 / 2],
+        [1 / 3, 0, 0, 0],
+        [1 / 3, 1 / 2, 0, 1 / 2],
+        [1 / 3, 1 / 2, 0, 0]
+    ], dtype=float)
 
-if __name__ == "__main__" :
-    print(vecteur_de_score(A1,0.15))
-    print("\n")
-    print(score_dynamique(A1,0.15,iter_max=300))
-    print("\n")
-    print(erreur_de_score(A1,0.15))
-    print("\n")
+    A2 = np.array([
+        [0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 1, 0]
+    ], dtype=float)
+    print("A1:\n")
+    print("Score  A1 :\n", vecteur_de_score(A1, 0.15), "\n")
+    print("Score (par les puissance) A1 :\n", score_dynamique(A1, 0.15, 300), "\n")
+    print("A2: \n")
+    print("Score  A2 :\n", vecteur_de_score(A2, 0.15), "\n")
+    print("Score (par les puissance) A2 :\n", score_dynamique(A2, 0.15, 300), "\n")
